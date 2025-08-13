6ac6ec9c639d60608160863505a930c5edb553a2
martymods <50667313+martymods@users.noreply.github.com>
2025-08-13 08:14:13 -0400

    Update twilioClient.js

--- services/twilioClient.js
+++ services/twilioClient.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
 const twilio = require('twilio');
-
-const client = twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 async function sendSMS(to, body) {
-  return client.messages.create({
-    to,
-    from: process.env.TWILIO_NUMBER,
-    body
-  });
+  const payload = { to, body };
+  if (process.env.MESSAGING_SERVICE_SID) {
+    payload.messagingServiceSid = process.env.MESSAGING_SERVICE_SID;
+  } else {
+    payload.from = process.env.TWILIO_NUMBER;
+  }
+  return client.messages.create(payload);
 }
 
 module.exports = { client, sendSMS };
